627917329c8f9f9f6b32ef46b7cb47e614e18274
Srujan1974 <147079802+Srujan1974@users.noreply.github.com>
2026-07-09 17:40:14 +0530

    Add files via upload

---
+++ JUnit_Basic Testing Exercises/Exercise 4 Arrange-Act-Assert (AAA) Pattern, Test Fixtures, Setup and Teardown Methods in JUnit/Exercise4-code/src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+        System.out.println("Setup executed");
+    }
+
+    @After
+    public void tearDown() {
+        calculator = null;
+        System.out.println("Teardown executed");
+    }
+
+    @Test
+    public void testAddition() {
+
+        // Arrange
+        int a = 10;
+        int b = 20;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(30, result);
+    }
+
+    @Test
+    public void testSubtraction() {
+
+        // Arrange
+        int a = 20;
+        int b = 5;
+
+        // Act
+        int result = calculator.subtract(a, b);
+
+        // Assert
+        assertEquals(15, result);
+    }
+
+}
