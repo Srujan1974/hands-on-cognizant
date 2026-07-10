d1d7c7c317a8a5b6f39f0d4b828a694abc3b592c
Srujan1974 <147079802+Srujan1974@users.noreply.github.com>
2026-07-10 16:16:17 +0530

    Add files via upload

---
+++ Mockito exercises/Exercise 2 Verifying Interactions/Exercise2-code/src/test/java/org/example/MyServiceTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+public class MyServiceTest {
+
+    @Test
+    void testVerifyInteraction() {
+
+        // Arrange
+        ExternalApi mockApi = mock(ExternalApi.class);
+        MyService service = new MyService(mockApi);
+
+        // Act
+        service.fetchData();
+
+        // Assert
+        verify(mockApi).getData();
+    }
+}
